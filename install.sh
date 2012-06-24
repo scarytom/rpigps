73634c77db68a3b3c89e7c5e128592614bb08d8b
scarytom <t.denley@cantab.net>
2012-06-24 17:39:15 +0100

    install shutdown hook

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ if (( EUID != 0 )); then
    exit 100
 fi
 
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 adduser --system rpigps
 
-cp $DIR/content/usr/bin/rpigps /usr/bin/rpigps
-
+cp $DIR/content/usr/bin/* /usr/bin/
+cp $DIR/content/etc/udev/rules.d/* /etc/udev/rules.d/
 cp $DIR/content/etc/init.d/rpigps /etc/init.d/rpigps
-update-rc.d rpigps defaults
 
+update-rc.d rpigps defaults
