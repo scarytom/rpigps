b3bbf88ab17013dda5af7e83e7b14f5eb331c22d
scarytom <t.denley@cantab.net>
2012-06-24 10:38:41 +0100

    install script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 adduser --system rpigps
 
+cp $DIR/content/usr/bin/rpigps /usr/bin/rpigps
+
 cp $DIR/content/etc/init.d/rpigps /etc/init.d/rpigps
 update-rc.d rpigps defaults
 
